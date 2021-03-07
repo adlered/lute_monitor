b76621b6f130d30ea0e80fcf92a79bd4f223929b
adler <1101635162@qq.com>
2021-03-07 11:00:07 +0800

    速率限制完成

--- src/main/java/pers/adlered/lute/monitor/Processor.java
+++ src/main/java/pers/adlered/lute/monitor/Processor.java
@@ -111,5 +111,4 @@ public class Processor implements Runnable {
             e.printStackTrace();
         }
     }
-
 }
--- src/main/java/pers/adlered/lute/monitor/Watcher.java
+++ src/main/java/pers/adlered/lute/monitor/Watcher.java
@@ -1,5 +1,7 @@
 package pers.adlered.lute.monitor;
 
+import pers.adlered.simplecurrentlimiter.main.SimpleCurrentLimiter;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,15 +16,24 @@ public class Watcher implements Runnable {
     @Override
     public void run() {
         try {
-            System.out.println("[LuteMonitor] Listening " + Vals.monitorProxyPort + " ...");
+            int access = Integer.parseInt(Vals.ipLimitOptions.split("/")[0]);
+            int second = Integer.parseInt(Vals.ipLimitOptions.split("/")[1]);
+            SimpleCurrentLimiter simpleCurrentLimiter = new SimpleCurrentLimiter(second, access);
+            System.out.println("[LuteMonitor] Limit Setting: " + access + " access in " + second + " second");
             ServerSocket serverSocket = new ServerSocket(Vals.monitorProxyPort);
+            System.out.println("[LuteMonitor] Listening " + Vals.monitorProxyPort + " ...");
             while (isRunning) {
                 Socket socket = serverSocket.accept();
                 // IP 频率审核与记录
-
-                // 核验通过，允许使用 Lute 服务
-                Processor processor = new Processor(socket);
-                new Thread(processor).start();
+                String ip = socket.getInetAddress().getHostAddress();
+                if (simpleCurrentLimiter.access(ip)) {
+                    // 核验通过，允许使用 Lute 服务
+                    Processor processor = new Processor(socket);
+                    new Thread(processor).start();
+                } else {
+                    socket.close();
+                    System.out.println("[Denied] " + ip);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
